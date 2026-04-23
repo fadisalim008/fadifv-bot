47990b01bcf0ced0701c5e6cabfa2002420d5955
fadisalim008 <swedish.song.of.the.day@gmail.com>
2026-04-24 00:58:03 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,3 +4,6 @@ class Config:
     API_ID = int(os.getenv("API_ID"))
     API_HASH = os.getenv("API_HASH")
     BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+    BOT_USERNAME = os.getenv("BOT_USERNAME")
+    OWNER_USERNAME = os.getenv("OWNER_USERNAME")
