f412980af11ff5566a46461b695eae984130748a
fadisalim008 <swedish.song.of.the.day@gmail.com>
2026-04-23 14:25:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,22 @@
 import telebot
 import json
+import os
 
-# 🔑 حط التوكن هنا
 bot = telebot.TeleBot("8667177884:AAFiV6hCpSX2AMyqi9apiiXo0UavZDNan74")
+
+os.makedirs("data", exist_ok=True)
+DATA_FILE = "data/users.json"
 
 def load_users():
     try:
-        with open("data/users.json", "r") as f:
+        with open(DATA_FILE, "r", encoding="utf-8") as f:
             return json.load(f)
     except:
         return {}
 
 def save_users(users):
-    with open("data/users.json", "w") as f:
-        json.dump(users, f, indent=4)
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(users, f, indent=4, ensure_ascii=False)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -28,5 +31,4 @@ def start(message):
 
     bot.reply_to(message, "تم تسجيلك 👍")
 
-# ▶️ تشغيل البوت
 bot.infinity_polling()
