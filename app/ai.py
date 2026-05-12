9a58bc9f953b7a1ff17a6e08471e0cf5c6a067ce
fadisalim008 <swedish.song.of.the.day@gmail.com>
2026-05-12 17:59:57 +0300

    Update ai.py

--- app/ai.py
+++ app/ai.py
@@ -1,1 +1,31 @@
+import requests
+from app.config import OPENROUTER_API_KEY
 
+def ask_ai(question):
+    try:
+        headers = {
+            "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "model": "deepseek/deepseek-chat",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": question
+                }
+            ]
+        }
+
+        r = requests.post(
+            "https://openrouter.ai/api/v1/chat/completions",
+            headers=headers,
+            json=data,
+            timeout=30
+        ).json()
+
+        return r["choices"][0]["message"]["content"]
+
+    except:
+        return "صار خطأ بالذكاء الصناعي"
