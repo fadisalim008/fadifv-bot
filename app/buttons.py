95be368e1160266b7b21c4dffdf49856b9d70dae
fadisalim008 <swedish.song.of.the.day@gmail.com>
2026-05-12 17:54:35 +0300

    Update buttons.py

--- app/buttons.py
+++ app/buttons.py
@@ -1,1 +1,37 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from app.config import BOT_USERNAME, DEV_USERNAME
 
+def source_button():
+    kb = InlineKeyboardMarkup()
+
+    kb.add(
+        InlineKeyboardButton(
+            "SOURCE FADI",
+            url=f"https://t.me/{DEV_USERNAME}"
+        )
+    )
+
+    return kb
+
+def start_buttons():
+    kb = InlineKeyboardMarkup(row_width=2)
+
+    kb.add(
+        InlineKeyboardButton(
+            "اضفني للكروب",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
+        )
+    )
+
+    kb.add(
+        InlineKeyboardButton(
+            "المطور",
+            url=f"https://t.me/{DEV_USERNAME}"
+        ),
+        InlineKeyboardButton(
+            "الاوامر",
+            callback_data="help"
+        )
+    )
+
+    return kb
