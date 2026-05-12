802367df93c6ff330950707c062bacae25b08abe
fadisalim008 <swedish.song.of.the.day@gmail.com>
2026-05-12 17:56:52 +0300

    Update music.py

--- app/music.py
+++ app/music.py
@@ -1,1 +1,43 @@
+import requests
+from app.buttons import source_button
 
+def search_song(query):
+    try:
+        url = "https://itunes.apple.com/search"
+
+        params = {
+            "term": query,
+            "limit": 1
+        }
+
+        r = requests.get(url, params=params).json()
+
+        if not r["results"]:
+            return None
+
+        song = r["results"][0]
+
+        return {
+            "name": song["trackName"],
+            "audio": song["previewUrl"]
+        }
+
+    except:
+        return None
+
+def send_music(bot, message, query):
+    song = search_song(query)
+
+    if not song:
+        return bot.reply_to(
+            message,
+            "صار خطأ أثناء جلب الأغنية"
+        )
+
+    bot.send_audio(
+        message.chat.id,
+        song["audio"],
+        title=song["name"],
+        reply_markup=source_button(),
+        reply_to_message_id=message.message_id
+    )
