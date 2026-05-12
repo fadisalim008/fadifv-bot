0872cf0bb3235de3c602830db3bf77f89908b631
fadisalim008 <swedish.song.of.the.day@gmail.com>
2026-05-12 17:58:20 +0300

    Update weather.py

--- app/weather.py
+++ app/weather.py
@@ -1,1 +1,28 @@
+import requests
 
+def get_weather(city):
+    try:
+        url = f"https://wttr.in/{city}?format=j1"
+
+        r = requests.get(url).json()
+
+        current = r["current_condition"][0]
+
+        temp = current["temp_C"]
+        feel = current["FeelsLikeC"]
+        humidity = current["humidity"]
+        wind = current["windspeedKmph"]
+        desc = current["weatherDesc"][0]["value"]
+
+        return f"""
+الطقس في {city}
+
+الحرارة: {temp}°C
+الاحساس: {feel}°C
+الحالة: {desc}
+الرطوبة: {humidity}%
+الرياح: {wind} km/h
+"""
+
+    except:
+        return "ماكدرت اجيب الطقس"
