ccc13d4ee2d63d9064ee1aa187e8809cec40eb21
fadisalim008 <swedish.song.of.the.day@gmail.com>
2026-05-12 17:49:03 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,18 @@
 import os
 
-class Config:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-    BOT_USERNAME = os.getenv("BOT_USERNAME")
-    OWNER_USERNAME = os.getenv("OWNER_USERNAME")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+
+BOT_USERNAME = os.getenv("BOT_USERNAME", "")
+
+DEV_USERNAME = os.getenv("DEV_USERNAME", "fvamv")
+
+FORCE_CHANNEL = os.getenv("FORCE_CHANNEL", "fadifva")
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "")
+
+START_PHOTO = os.getenv(
+    "START_PHOTO",
+    "https://i.ibb.co/xqVzNV7t/db72f6d6-2b6a-4f58-abdc-2f47a3aeb664.jpg"
+)
